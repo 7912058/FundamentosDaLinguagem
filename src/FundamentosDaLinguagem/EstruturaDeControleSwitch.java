334e8fb536c898aec89acc07ba69bd255cd2e046
Richard Leonardo <paulino_richard@hotmail.com>
2022-07-27 02:15:31 -0300

    Commit

--- src/FundamentosDaLinguagem/EstruturaDeControleSwitch.java
+++ src/FundamentosDaLinguagem/EstruturaDeControleSwitch.java
@@ -8,5 +8,17 @@ public class EstruturaDeControleSwitch {
 
         System.out.println("Digite o final da placa: ");
         int finalPlaca = entrada.nextInt();
+
+        switch (finalPlaca) {
+            case 1:
+                System.out.println("Vencimento dia 11 de janeiro.");
+                break;
+            case 2:
+                System.out.println("Vencimento dia 12 de fevereiro.");
+                break;
+
+            default:
+                System.out.println("Opcao invalida.");
+        }
     }
 }
---
+++ src/FundamentosDaLinguagem/IncrementoDecremento.java
@@ -0,0 +1,12 @@
+package FundamentosDaLinguagem;
+
+public class IncrementoDecremento {
+    public static void main(String[] args) {
+        int idade = 10;
+
+        int novaIdade = --idade;
+
+        System.out.println("Idade: " + idade);
+        System.out.println("Nova idade: " + novaIdade);
+    }
+}
