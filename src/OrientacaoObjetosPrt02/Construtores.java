cb2d9e2efc91d8451d5c152dc293da6824b654be
Richard Leonardo <paulino_richard@hotmail.com>
2022-08-25 17:41:28 -0300

    Build in 25/08/22 17:40...

--- src/OrientacaoObjetosPrt02/Construtores.java
+++ src/OrientacaoObjetosPrt02/Construtores.java
@@ -2,12 +2,10 @@ package OrientacaoObjetosPrt02;
 
 public class Construtores {
     public static void main(String[] args) {
-        Pessoa p1 = new Pessoa("Joao");
-
+        Pessoa p1 = new Pessoa("Joao", 0);
         System.out.println("Nome: \"" + p1.nome + "\" tem " +  p1.idade + " anos.");
 
-        p1.idade = 25;
-
-        System.out.println("Nome: \"" + p1.nome + "\" tem " + p1.idade + " anos.");
+        Pessoa p2 = new Pessoa("Maria", 22);
+        System.out.println("Nome: \"" + p2.nome + "\" tem " + p2.idade + " anos.");
     }
 }
--- src/OrientacaoObjetosPrt02/Pessoa.java
+++ src/OrientacaoObjetosPrt02/Pessoa.java
@@ -3,9 +3,15 @@ package OrientacaoObjetosPrt02;
 public class Pessoa {
 
     String nome;
-    int idade;
+    Integer idade;
 
     public Pessoa(String pessoa) {
         this.nome = nome;
+        // Se tiver uma inicialização bem complicada de fazer
+    }
+
+    Pessoa(String nome, int idade) {
+        this(nome) ; // Deve ser a primeira instrução dentro do construtor.
+        this.idade = idade;
     }
 }
